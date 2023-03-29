06b56d33a941722f6f1e0d4ddce2b06e9b8096e1
ckwjdgur12 <jhcha@knu.ac.kr>
2023-03-29 19:14:27 +0900

    Add comments

--- BOJ/Gold/G3/1520.py
+++ BOJ/Gold/G3/1520.py
@@ -12,23 +12,23 @@ dy = [0, 0, 1, -1]
 
 def dfs(x, y):
 
-    if (x == M-1 and y == N-1): return 1
-    if dp[x][y] != -1: return dp[x][y]
+    if (x == M-1 and y == N-1): return 1    # 도착지에 도착하면 1 return
+    if dp[x][y] != -1: return dp[x][y]      # 방문 했던 지점이면 그 지점 값 리턴
 
     dp[x][y] = 0
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if not (0 <= nx < M and 0 <= ny < N): continue
-        if MAP[nx][ny] >= MAP[x][y]: continue
-        if MAP[nx][ny] < MAP[M-1][N-1]: continue
+        if not (0 <= nx < M and 0 <= ny < N): continue  # 범위를 벗어나면 continue
+        if MAP[nx][ny] >= MAP[x][y]: continue           # 현재 높이보다 같거나 높으면 continue
+        if MAP[nx][ny] < MAP[M-1][N-1]: continue        # 도착 지점보다 낮으면 continue
 
-        dp[x][y] += dfs(nx, ny)
+        dp[x][y] += dfs(nx, ny) # 현재 지점까지의 branch 개수 모으기
 
-    return dp[x][y]
+    return dp[x][y] # 더이상 탐색할 수 있는 곳이 없다면 자기자신 리턴
+
 
 print((dfs(0, 0)))
-
 
 
 
