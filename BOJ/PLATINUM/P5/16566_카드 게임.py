c863121481281fd25b5f1aa48331cac17ea9058d
ckwjdgur12 <jhcha@knu.ac.kr>
2023-07-05 21:13:51 +0900

    using union-find

--- BOJ/PLATINUM/P5/16566_카드 게임.py
+++ BOJ/PLATINUM/P5/16566_카드 게임.py
@@ -1,13 +1,43 @@
 import sys
+import bisect
 import math
 # input = sys.stdin.readline
 sys.stdin = open("BOJ/input.txt", "r")
+
+# def findParent(x):
+#     if parent[x] != x:
+#         parent[x] = findParent(parent[x])
+#     return parent[x]
+
+# def unionParent(x, y):
+#     x = findParent(x)
+#     y = findParent(y)
+#     parent[x] = y
+
+
+# N, M, K = map(int, input().split())
+
+# myCards = list(map(int, input().split()))
+# opponentCards = list(map(int, input().split()))
+# parent = list(i for i in range(len(myCards)+1))
+
+# myCards.sort()
+# for opponentCard in opponentCards:
+#     nextCard = opponentCard + 1
+
+#     idx = bisect.bisect_left(myCards, nextCard)
+
+#     value = findParent(idx)
+#     print(myCards[value])
+#     unionParent(value, value+1)
+
+
 
 N, M, K = map(int, input().split())
 
 myCards = list(map(int, input().split()))
 opponentCards = list(map(int, input().split()))
-idxNum = int(math.sqrt(4000000)) + 1
+idxNum = int(math.sqrt(50000)) + 1
 check = [False] * 4000001
 dummy = [0] * idxNum
 
